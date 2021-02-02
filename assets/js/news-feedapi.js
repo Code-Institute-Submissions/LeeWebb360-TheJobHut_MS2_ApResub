240de1163cfd1a616c7764f1363e252b80607f85
LeeWebb360 <lee.webb2015@gmail.com>
2021-02-02 00:12:06 +0000

    added thumbnails to api and extracted images into carousel with jQuery

--- assets/js/news-feedapi.js
+++ assets/js/news-feedapi.js
@@ -4,9 +4,8 @@ $(document).ready(function () {
     const settings = {
         "async": true,
         "crossDomain": true,
-        "url": "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI?q=employment%20uk&pageNumber=1&pageSize=10&autoCorrect=true&fromPublishedDate=null&toPublishedDate=null",
+        "url": "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI?q=employmentuk&pageNumber=1&pageSize=10&autoCorrect=true&withThumbnails=true&fromPublishedDate=null&toPublishedDate=null",
         "method": "GET",
-        "dataType": "Json",
         "headers": {
             "x-rapidapi-key": "d3c86e5140mshf9ee7714ea78605p16d425jsnf445de32948d",
             "x-rapidapi-host": "contextualwebsearch-websearch-v1.p.rapidapi.com"
@@ -16,31 +15,34 @@ $(document).ready(function () {
     $.ajax(settings).done(function (response) {
         console.log(response.value);
 
+
         let cardArray = [];
         let output = "";
         let latestNews = response.value;
+        let post_limit = 2;
+        let materializeColWidth = 12/post_limit;
 
         for (var i in latestNews) {
             output += `
     <div class="carousel-cell">
-    <div class="col">   
-    <div class="card hoverable">
-    ${latestNews[i].image.url ?
-      `<div class="card-image">
-        <img src="${latestNews[i].image.webpageUrl}" class="responsive-img">
-      </div>`:null}
+    <div class="col 1${materializeColWidth} m6 s12">   
+    <div class="card medium hoverable">
+    ${latestNews[i].image.thumbnail ?
+                    `<div class="card-image">
+        <img src="${latestNews[i].image.thumbnail}" class="responsive-img">
+      </div>`: null}
       <div class="card-content">
-      <span class="card-title"><i class="material">more_vert</i></span>
+      <span class="card-title activator"><i class="material-icons right">more_vert</i></span>
       <h6>${latestNews[i].title}</h6>
     </div>
 
     <div class="card-reveal">
-    <span class="card-title activator"><i class="material-icons right">more_vert</i></span>
+    <span class="card-title activator"><i class="material-icons right">close</i></span>
     <p>${latestNews[i].description}</p>
     </div>
     
     <div class="card-action">
-    <a href="${latestNews[i].url}" target="_blank" class="btn">Read More</a>
+    <a href="${latestNews[i].url}" target="_blank" class="btn btn-primary btn-lg">Read More</a>
     </div>
     </div>
     </div>
@@ -60,7 +62,9 @@ $(document).ready(function () {
                     freeScroll: true,
                     wrapAround: true,
                     pageDots: false,
-                    prevNextButtons: false
+                    prevNextButtons: false,
+                    imagesLoaded: true,
+                    resize: true
                 });
             }, 0);
 
