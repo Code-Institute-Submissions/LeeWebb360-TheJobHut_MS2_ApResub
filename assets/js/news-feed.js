f3918802b92c4b59704224c38f00b0f5ca9f61fb
LeeWebb360 <lee.webb2015@gmail.com>
2021-02-03 13:09:26 +0000

    added second api and news-feed.js file with functionality to pull data from the api

--- assets/js/news-feed.js
+++ assets/js/news-feed.js
@@ -1,71 +1,49 @@
-//document ready function CREDIT STACKOVERFLOW 
-//https://stackoverflow.com/questions/9899372/pure-javascript-equivalent-of-jquerys-ready-how-to-call-a-function-when-t
+$(document).ready(function () {
+
+    $("#search").on("click", function (e) {
+        e.preventDefault();
+
+        let query = $("#searchquery").val();
+        let url = "https://newscatcher.p.rapidapi.com/v1/search?q=" + query + "&lang=en&sort_by=relevancy&page=1&media=True";
+
+        if (query !== "") {
+
+            $.ajax({
+                url: url,
+                method: "GET",
+                dataType: "json",
+                headers: {
+                    "x-rapidapi-key": "d3c86e5140mshf9ee7714ea78605p16d425jsnf445de32948d",
+                    "x-rapidapi-host": "newscatcher.p.rapidapi.com"
+                },
+                
+
+                beforeSend: function(){
+                    $("#loader").show();
+                },
+
+                complete: function(){
+                    $("#loader").hide();
+                },
+
+                success: function(data){
+                    console.log(data);
+                },
+
+                error: function(){
+                    console.log("Error");
+                }
+
+            });
 
 
-(function (funcName, baseObj) {
-    funcName = funcName || "docReady";
-    baseObj = baseObj || window;
-    var readyList = [];
-    var readyFired = false;
-    var readyEventHandlersInstalled = false;
-
-
-    function ready() {
-        if (!readyFired) {
-            readyFired = true;
-            for (var i = 0; i < readyList.length; i++) {
-                readyList[i].fn.call(window, readyList[i].ctx);
-            }
-
-            readyList = [];
-        }
-    }
-
-    function readyStateChange() {
-        if (document.readyState === "complete") {
-            ready();
-        }
-    }
-
-    baseObj[funcName] = function (callback, context) {
-        if (typeof callback !== "function") {
-            throw new TypeError("callback for docReady(fn) must be a function");
-        }
-        if (readyFired) {
-            setTimeout(function () { callback(context); }, 1);
-            return;
         } else {
-            readyList.push({ fn: callback, ctx: context });
+            console.log("please enter something")
         }
-        if (document.readyState === "complete") {
-            setTimeout(ready, 1);
-        } else if (!readyEventHandlersInstalled) {
-            if (document.addEventListener) {
-                document.addEventListener("DOMContentLoaded", ready, false);
-                window.addEventListener("load", ready, false);
-            } else {
-                document.attachEvent("onreadystatechange", readyStateChange);
-                window.attachEvent("onload", ready);
-            }
-            readyEventHandlersInstalled = true;
-        }
-    }
-})("docReady", window);
 
-//document ready function end
 
-fetch("https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI?q=jobs&pageNumber=1&pageSize=10&autoCorrect=true&fromPublishedDate=null&toPublishedDate=null", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "d3c86e5140mshf9ee7714ea78605p16d425jsnf445de32948d",
-		"x-rapidapi-host": "contextualwebsearch-websearch-v1.p.rapidapi.com"
-	}
-}).then((res)=>{
-    return res.json()
-}).then((data)=>{
-    console.dir(data.value)
+
+    });
+
 
 })
-
-
-
