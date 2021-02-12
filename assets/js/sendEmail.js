74d31cb0d69e335319457d8cadfb85aef6ecb6f2
LeeWebb360 <lee.webb2015@gmail.com>
2021-02-12 04:57:18 +0000

    correct form inputs acros the site to be repsonsive. added sendEmail.js and functions to send emails from aboutus.html

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,16 @@
+function sendMail(contactForm) {
+    emailjs.send('service_m2ymfqq', 'Thejobhut', {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "new_message": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        });
+        return false;
+}
+ 
